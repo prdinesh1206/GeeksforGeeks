2172250f231658194867ac70cafcd2d28e762d97
prdinesh1206 <prdinesh1206@gmail.com>
2017-08-20 15:28:28 -0400

    Added program for longest valid parenthesis.

--- src/LongestValidParenthesis.java
+++ src/LongestValidParenthesis.java
@@ -2,8 +2,8 @@ import java.util.Stack;
 
 public class LongestValidParenthesis {
 	public static void main(String[] args){
-		LongestValidParenthesis ans = new LongestValidParenthesis();
-		int x= ans.longestValidParentheses("()(((((())())((()())(())");
+		LongestValidParenthesis ll = new LongestValidParenthesis();
+		int x = ll.longestValidParentheses("(())))");
 		System.out.println(x);
 	}
 	public int longestValidParentheses(String a) {
@@ -11,30 +11,31 @@ public class LongestValidParenthesis {
 	    if(l<=1){
 	        return 0;
 	    }
-	    Stack<Character> stack = new Stack<Character>();
+	    Stack<Integer> stack = new Stack<Integer>();
+	    stack.push(-1);
 	    int cur=0, max=0;
 	    for(int i=0;i<l;i++){
 	        char ch=a.charAt(i);
 	        if(ch=='('){
-	            stack.push(ch);
+	            stack.push(i);
 	        }else{
+	            stack.pop();
 	            if(!stack.empty()){
-	                char re=stack.pop();
-	                
-	                cur=cur+2;
-	                if(max<cur){
-	                	max=cur;
-	                }
+	                max = Math.max(max, i-stack.peek());
 	            }else{
-	                if(max<cur){
-	                    max=cur;
-	                }
-	                cur=0;
+	                stack.push(i);
 	            }
 	        }
-	        System.out.println(stack+" "+max);
 	    }
 	    return max;
+	}
+	public int nextIndex(int l, String a){
+	    for(int i=l;i>=0;i--){
+	        if(a.charAt(i)==')'){
+	            return i;
+	        }
+	    }
+	    return -1;
 	}
 	public void printList(int[][] list, int l){
 	    for(int i=0;i<l+1;i++){
@@ -44,5 +45,4 @@ public class LongestValidParenthesis {
 	        System.out.println();
 	    }
 	}
-
 }
